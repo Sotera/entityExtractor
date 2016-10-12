340cc81b05dfb516c83b8ddc03c87ee3670a79fa
Luke Wendling <luke@lukewendling.com>
2016-10-12 12:42:34 -0500

    dir util: chmod new dir so shared services can read-write

--- services/util/dirtools.py
+++ services/util/dirtools.py
@@ -1,10 +1,11 @@
 # dir helpers
-import os, errno
+import os, errno, stat
 
 def mkdir_p(path):
     """ 'mkdir -p' in Python > 2.5"""
     try:
-        os.makedirs(path, 0777)
+        os.makedirs(path)
+        os.chmod(path, 0777)
     except OSError as exc:
         # pass if already exists
         if exc.errno == errno.EEXIST and os.path.isdir(path):
