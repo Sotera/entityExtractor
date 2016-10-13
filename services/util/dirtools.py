e44d306f6256e6e32f72ffe6586ae20e59486beb
Luke Wendling <luke@lukewendling.com>
2016-10-13 14:30:26 -0500

    services/util now python3 friendly

--- services/util/dirtools.py
+++ services/util/dirtools.py
@@ -5,7 +5,8 @@ def mkdir_p(path):
     """ 'mkdir -p' in Python > 2.5"""
     try:
         os.makedirs(path)
-        os.chmod(path, 0777)
+        # 0o = py3 octal
+        os.chmod(path, 0o777)
     except OSError as exc:
         # pass if already exists
         if exc.errno == errno.EEXIST and os.path.isdir(path):
