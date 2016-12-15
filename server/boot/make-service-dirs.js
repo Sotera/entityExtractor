a853c79a7df2ba4129f28ef6acbf69fc91d848e5
Luke Wendling <luke@lukewendling.com>
2016-12-14 20:34:42 -0600

    cleanup

--- server/boot/make-service-dirs.js
+++ server/boot/make-service-dirs.js
@@ -6,7 +6,7 @@ const mkdirp = require('mkdirp');
 module.exports = function(app) {
   const oldMask = process.umask();
   process.umask(0); // https://github.com/nodejs/node-v0.x-archive/issues/7591
-  ['features', 'threats', 'image-fetcher', Date.now()].forEach(dir => {
+  ['features', 'threats', 'image-fetcher'].forEach(dir => {
     mkdirp.sync(`/downloads/${dir}`, parseInt('0777', 8));
   });
   process.umask(oldMask); // reset
