db6138b1105657a630bb0e1fbc31bfb32e827595
kenglish-pfi <kevin.english@soteradefense.com>
2016-12-22 09:33:30 -0600

    Added absentfriends domain name clustering service to firmament container list.

--- services/aggregate-clustering/aggregator.py
+++ services/aggregate-clustering/aggregator.py
@@ -35,7 +35,7 @@ def process(job):
 
                 if aggregation:
                     # hashtags don't calc average_similarity_vector
-                    if job['data_type'] != 'hashtag':
+                    if job['data_type'] not in ['hashtag', 'domain']:
                         agg_cluster['average_similarity_vector'] = aggregation.average_similarity_vector
 
                     agg_cluster['end_time_ms'] = posts_cluster['end_time_ms']
