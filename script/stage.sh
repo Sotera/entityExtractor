e03ccf03fc7f2d3caf291724cefd3adcd8cb5067
Justin Lueders <justin.lueders@gmail.com>
2017-05-18 16:57:28 -0500

    add tunnel port to options

--- script/stage.sh
+++ script/stage.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 ## example call
-## ./stage.sh -h 1.1.1.1 -s 1490729833207 -e 1490731633206 -i "../../../.ssh/MyKeyFile.pem" -p 27017
+## ./stage.sh -h 1.1.1.1 -s 1490729833207 -e 1490731633206 -i "../../../.ssh/MyKeyFile.pem" -p 27017 -l 1234
 
 ## assume we are using rancor...cause we are
 REMOTE_DB="rancor"
 LOCAL_DB="rancor"
 
-usage="$(basename "$0") [-h] [-i] [-r] [-p] [-s] [-e] -- stage the a local watchman instance from data on a remote instance
+usage="$(basename "$0") [-h] [-i] [-r] [-p] [-l] [-s] [-e] -- stage the a local watchman instance from data on a remote instance
 
 where:
     -h  print this text
     -i  path to ssh key file
     -r  remote mongodb host (ip)
     -p  mongodb port on the remote host
+    -l  local tunnel port (a free one to use for the tunnel)
     -s  start time in ms
     -e  end time in ms"
 
@@ -22,8 +23,9 @@ r_flag=false
 p_flag=false
 s_flag=false
 e_flag=false
+l_flag=false
 
-while getopts ':hi:r:p:s:e:' option
+while getopts ':hi:r:p:l:s:e:' option
 do
    case $option in
      h) echo "$usage"
@@ -38,6 +40,9 @@ do
      p) PORT=$OPTARG
         p_flag=true
         ;;
+     l) TUNNEL_PORT=$OPTARG
+             l_flag=true
+             ;;
      s) START_TIME_MS=$OPTARG
         s_flag=true
         ;;
@@ -51,7 +56,7 @@ do
 done
 shift $(($OPTIND - 1))
 
-if [ $i_flag = false ] || [ $r_flag = false ] || [ $p_flag = false ] || [ $s_flag = false ] || [ $e_flag = false ]
+if [ $i_flag = false ] || [ $r_flag = false ] || [ $p_flag = false ] || [ $l_flag = false ] || [ $s_flag = false ] || [ $e_flag = false ]
 then
     echo  "
 ****** ALL FLAGS MUST BE PROVIDED! *****"
@@ -67,6 +72,10 @@ then
     then
       echo "*** missing -p"
     fi
+    if [ $l_flag = false ]
+    then
+      echo "*** missing -l"
+    fi
     if [ $s_flag = false ]
     then
       echo "*** missing -s"
@@ -81,7 +90,7 @@ then
 fi
 
 ## add -v to get verbose info to debug ssh connection
-ssh -M -S my-ctrl-socket -i $KEY_FILE_PATH ubuntu@$HOST -L 9999:localhost:$PORT -fnNT
+ssh -M -S my-ctrl-socket -i $KEY_FILE_PATH ubuntu@$HOST -L $TUNNEL_PORT:localhost:$PORT -fnNT
 ssh -S my-ctrl-socket -O ubuntu@$HOST
 
 ##Query Example
@@ -89,20 +98,20 @@ ssh -S my-ctrl-socket -O ubuntu@$HOST
 
 ## DUMP THE REMOTE SETTING COLLECTION
 echo "Dumping '$HOST:$PORT/$REMOTE_DB/setting'..."
-echo mongodump --host localhost:9999 --db $REMOTE_DB --collection "setting"
-mongodump --host localhost:9999 --db $REMOTE_DB --collection "setting"
+echo mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --collection "setting"
+mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --collection "setting"
 
 ## DUMP THE REMOTE SOCIAL MEDIA POST COLLECTION
 QUERY_SOCIAL_MEDIA_POST="{'timestamp_ms':{\$gte:$START_TIME_MS,\$lte:$END_TIME_MS}}"
 echo "Dumping '$HOST:$PORT/$REMOTE_DB/SocialMediaPost'..."
-echo mongodump --host localhost:9999 --db $REMOTE_DB --query $QUERY_SOCIAL_MEDIA_POST --collection "socialMediaPost"
-mongodump --host localhost:9999 --db $REMOTE_DB --query $QUERY_SOCIAL_MEDIA_POST --collection "socialMediaPost"
+echo mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --query $QUERY_SOCIAL_MEDIA_POST --collection "socialMediaPost"
+mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --query $QUERY_SOCIAL_MEDIA_POST --collection "socialMediaPost"
 
 ## DUMP THE REMOTE POSTS CLUSTER COLLECTION
 QUERY_POSTS_CLUSTER="{'start_time_ms':{\$gte:$(expr $START_TIME_MS - 604800000),\$lte:$START_TIME_MS}}"
 echo "Dumping '$HOST:$PORT/$REMOTE_DB/postsClusters'..."
-echo mongodump --host localhost:9999 --db $REMOTE_DB --query $QUERY_POSTS_CLUSTER --collection "postsCluster"
-mongodump --host localhost:9999 --db $REMOTE_DB --query $QUERY_POSTS_CLUSTER --collection "postsCluster"
+echo mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --query $QUERY_POSTS_CLUSTER --collection "postsCluster"
+mongodump --host localhost:$TUNNEL_PORT --db $REMOTE_DB --query $QUERY_POSTS_CLUSTER --collection "postsCluster"
 
 ssh -S my-ctrl-socket -O exit ubuntu@$HOST
 
