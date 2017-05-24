d500d6d12d38ea3c3b2f3a555166a350b86ff91c
Luke Wendling <luke@lukewendling.com>
2017-05-24 11:23:01 -0500

    rm dupe check in qcr endpoint

--- common/models/qcr.js
+++ common/models/qcr.js
@@ -4,39 +4,8 @@ const _ = require('lodash'),
   preprocessor = require('../../lib/preprocessors/twitter'),
   debug = require('debug')('qcr');
 
-// def: QCR endpoint to inspect post data
-// and create SocialMediaPost entries.
+// def: QCR endpoint to inspect post data and create SocialMediaPost entries.
 module.exports = function(Qcr) {
-
-  let bootTime = Date.now();
-  let postWindowStart = bootTime;
-  let postWindowPostCount = 0;
-  let postWindowInterval = 1000;
-  let postPerSecondTarget = +process.env.PPS_TARGET || -1;
-  let postPerSecondDelta = 0;
-  let postPerSecondCount = 0;
-  let filteredPostCount = 0;
-  let fpps = 0;
-  let pps = 0;
-  let failures = 0;
-  let postsIgnored = 0;
-  let dupeSet = new Set();
-  let postAge = 0;
-  let dupeInterval = +process.env.DUPE_INTERVAL_MS || 10000;
-
-  // ignore posts before this
-  let daysBack = +process.env.IGNORE_DAYS_BACK || 3;
-  let secondsBack = daysBack * 24 * 60 * 60;
-
-  if(postPerSecondTarget < 0)
-    debug('Filter off!');
-  else
-    debug('post per second target:', postPerSecondTarget);
-
-  let interval = setInterval(() => {
-    debug("clearing dupe set...");
-    dupeSet.clear();
-  }, dupeInterval);
 
   Qcr.remoteMethod(
     'insert',
@@ -53,28 +22,6 @@ module.exports = function(Qcr) {
     }
   );
 
-  setInterval(function(){
-    pps = postPerSecondCount / ((Date.now() - bootTime) / 1000);
-    debug('--==PPS==--:' + pps);
-    if(postPerSecondTarget>=0) {
-      fpps = filteredPostCount / ((Date.now() - bootTime) / 1000);
-      debug("--==FPPS==--:" + fpps + " Delta:" + postPerSecondDelta);
-    }
-    let avgAge = postAge/postPerSecondCount;
-    postPerSecondCount = 0;
-    filteredPostCount = 0;
-    bootTime = Date.now();
-    postAge = 0;
-
-    debug("qcr dupes over the last 5 seconds: " + failures);
-    debug("dupes ignored over the last 5 seconds: " + postsIgnored);
-    debug("dupe list size:" + dupeSet.size);
-    debug("Average Post Age: " + avgAge);
-
-    postsIgnored = 0;
-    failures = 0;
-  }, 5000);
-
   Qcr.insert = function(req, cb) {
     const attrs = req.body;
 
@@ -82,42 +29,6 @@ module.exports = function(Qcr) {
     if (+process.env.IGNORE_QCR) {
       return cb(null, attrs); // send 200 code and stop
     }
-
-    //--------------------------------------------------------------------------
-    let postId = attrs['post_id'];
-    if(dupeSet.has(postId)){
-      postsIgnored++;
-      return cb(null, attrs); // send 200 code and stop
-    }
-    dupeSet.add(postId);
-    //--------------------------------------------------------------------------
-
-    if((Date.now() - new Date(attrs['timestamp_ms']).getTime())/1000 > secondsBack)
-      return cb(null, attrs);
-
-    //TODO: less hacky!
-    //Calculating our posts per second so we get a real amount to deal with
-    postPerSecondCount ++;
-    postAge += (Date.now() - new Date(attrs['timestamp_ms']).getTime())/1000;
-    if(postPerSecondTarget >=0){
-      postWindowPostCount++;
-      if(Date.now() >= postWindowStart + postWindowInterval){
-        postWindowStart = Date.now();
-
-        if((fpps - pps) < -.5 && (fpps - postPerSecondTarget) < -.5){
-          postPerSecondDelta ++;
-        }
-        if((fpps - postPerSecondTarget) > .5) postPerSecondDelta --;
-
-        postWindowPostCount = 0;
-      }
-      if(postWindowPostCount > postPerSecondTarget + postPerSecondDelta){
-        return cb(null, attrs);
-      }
-
-      filteredPostCount++;
-    }
-    //TODO: less hacky!
 
     let qcrData = new Qcr(attrs);
 
@@ -173,7 +84,6 @@ module.exports = function(Qcr) {
       // b/c they send lots of dupe tweets, we get mongo uniq idx failures.
       // ignore them.
       debug(err);
-      failures++;
       cb(null, {ok: 1}); // send bogus 200 response
     });
   }
