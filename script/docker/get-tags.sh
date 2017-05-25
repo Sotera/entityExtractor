a104dddba5bb6fe8692d1ad67b22035d8b8fe49d
Luke Wendling <luke@lukewendling.com>
2017-05-25 11:14:16 -0500

    bump dr-man

--- script/docker/get-tags.sh
+++ script/docker/get-tags.sh
@@ -7,7 +7,7 @@ if ! [ -e "$jq" ]; then
   exit $?
 fi
 
-for name in 'absentfriends' 'caffe_redis_client' 'comedian' 'dr-manhattan' \
+for name in 'caffe_redis_client' 'comedian' 'dr-manhattan' \
 'feature-sim' 'image-fetcher' 'ozymandias' 'rorschach'
 do
   curl -s https://registry.hub.docker.com/v2/repositories/sotera/$name/tags/ |
