84c5b9fd96f8386e4c27417848697e672d5dac27
Justin Lueders <justin.lueders@gmail.com>
2017-06-08 15:16:37 -0500

    create a location extraction service

---
+++ services/location-extractor/locate.py
@@ -0,0 +1,99 @@
+import sys, os, traceback
+sys.path.append(os.path.join(os.path.dirname(__file__), "../util"))
+from text_utils import remove_punctuation
+from entity_extractor import EntityExtractor
+from loopy import Loopy
+
+# big list: load it once
+stop_path = os.path.join(os.path.dirname(__file__), 'files', 'stopWordList.txt')
+stop_file = open(stop_path, 'r')
+stop_list = {w.strip('\n').strip('\r') for w in stop_file}
+ent_ext = EntityExtractor()
+
+def max_pair(d):
+    v = list(d.values())
+    k = list(d.keys())
+    return d[k[v.index(max(v))]]
+
+
+class Locate:
+    def __init__(self, base_url, geo_url, geo_threshold):
+        self.geo_url = geo_url
+        self.geo_threshold = geo_threshold
+
+        if base_url[-1] == '/':
+            self.url = base_url
+        else:
+            self.url = base_url + '/'
+
+    def get_location(self, text):
+        locs = ent_ext.extract(text, tag='I-LOC')
+        location = {}
+        places = []
+        for loc in locs:
+            print 'Location:', loc.encode('utf-8')
+            try:
+                geos = Loopy.post(self.geo_url, json={'address': loc})
+                for place in geos:
+                    places.append(place)
+                    break
+            except Exception as e:
+                print "error getting locations from geocoder...continuing.", e
+                traceback.print_exc()
+
+        for place in places:
+            if type(place) is not dict:
+                continue
+            place_name = ''
+            street_name = ''
+            zipcode = ''
+            city = ''
+            state = ''
+            country = ''
+            weight = 0.0
+
+            if 'streetName' in place.keys():
+                place_name = place['streetName'] + ' '
+                street_name = place['streetName']
+                weight += 2
+            if 'zipcode' in place.keys():
+                place_name = place['zipcode'] + ' '
+                zipcode = place['zipcode']
+                weight += 1.5
+            if 'city' in place.keys():
+                place_name = place['city'] + ' '
+                city = place['city']
+                weight += 1
+            if 'state' in place.keys():
+                place_name += place['state'] + ' '
+                state = place['state']
+                weight += .1
+            if 'country' in place.keys():
+                place_name += ' ' + place['country'] + ' '
+                country = place['country']
+                weight += .05
+            if place_name in location:
+                location[place_name]['weight'] += weight
+                location[place_name]['street_name'] = street_name
+                location[place_name]['zipcode'] = zipcode
+                location[place_name]['city'] = city
+                location[place_name]['state'] = state
+                location[place_name]['country'] = country
+            else:
+                location[place_name] = {
+                    "street_name": street_name,
+                    "zipcode": zipcode,
+                    "city": city,
+                    "state": state,
+                    "country": country,
+                    "type": "inferred point",
+                    "geo_type": "point",
+                    "weight": weight,
+                    "coords": {
+                        "lat": place['latitude'],
+                        "lng": place['longitude']
+                    }
+                }
+
+            location = dict((k, v) for k, v in location.items() if v['weight'] >= self.geo_threshold)
+            return max_pair(location)
---
+++ services/location-extractor/main.py
@@ -0,0 +1,53 @@
+from flask import request, abort, Flask
+from json import dumps as jd
+from locate import Locate
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../util'))
+
+api_root = os.getenv('API_ROOT', 'http://172.17.0.1:3003/api')
+if api_root[-1] != '/':
+    api_root += '/'
+
+app = Flask('Location Extraction Service')
+
+
+def set_err(job, msg):
+    job['state'] = 'error'
+    job['error'] = msg
+
+
+def err_check(job):
+    required = {'text'}
+    if not required.issubset(job):
+        set_err(job, 'Missing some required fields {}'.format(required))
+
+
+def process_message(job):
+    global api_root
+    err_check(job)
+    if 'state' in job and job['state'] == 'error':
+        return
+
+    geo_threshold = 0.0 if 'geo_threshold' not in job else float(job['geo_threshold'])
+
+    locate = Locate(api_root, '{}geocoder/forward-geo'.format(api_root), geo_threshold)
+    job['location'] = locate.get_location(job['text'])
+    return jd(job)
+
+
+@app.route('/extract', methods=['POST'])
+def extract():
+    if not request.json or not 'text' in request.json:
+        abort(400)
+    return process_message(request.json), 200
+
+
+@app.route('/')
+def index():
+    return 'Server Active.'
+
+
+if __name__ == '__main__':
+    app.run()
+# job = {'state': 'new', 'api_root': 'http://172.17.0.1:3003/api',
+#           'text': 'New poll: 6 out of 10 Americans support the Paris Agreement. Are you one of them? Join the movement.'}
