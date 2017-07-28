aed9d2257ebe6882c654f2ef4372061efd1a8cde
Luke Wendling <luke@lukewendling.com>
2017-07-28 10:37:54 -0500

    start-pyspark can start in local or cluster mode

--- script/docker/start-pyspark.sh
+++ script/docker/start-pyspark.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# default: run spark in local mode.
+svc="pyspark-nb"
+
+# if "cluster", start master, worker containers.
+if [ "$1" == "cluster" ]; then
+  svc=""
+  echo "** You can run spark in local or cluster mode (spark://master:7077) **"
+else
+  echo "** You can run spark in local mode only **"
+fi
+
 cd deploy/docker/pyspark-mongo-jupyter
 [ -d data ] || mkdir data
-docker-compose -f docker-compose.notebook.yml up
+docker-compose -f docker-compose.notebook.yml up $svc
