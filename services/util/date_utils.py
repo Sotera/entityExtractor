b9a0eab3bbc0cdc8fa182fe65c48868052f2efb5
Luke Wendling <luke@lukewendling.com>
2017-08-29 14:55:39 -0500

    py3 date_utils

--- services/util/date_utils.py
+++ services/util/date_utils.py
@@ -16,7 +16,7 @@ def str_to_dt(str_dt, form=1):
     if form==1: #ex: 2014-08-10T01:59:31.979Z - GNIP Raw data, mongo SMP "created"
         return datetime.strptime(str_dt[:-5], "%Y-%m-%dT%H:%M:%S")
     else:
-        print "Unrecongnized format"
+        print("Unrecongnized format")
         return None
 
 def ms_time_interval(dt1, dt2):
