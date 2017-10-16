40f02bbf99e6adbea1da749da0e52ce9c731e443
Luke Wendling <luke@lukewendling.com>
2017-10-16 11:03:09 -0500

    update nb_utils

--- services/util/nb_utils.py
+++ services/util/nb_utils.py
@@ -1,6 +1,7 @@
 # misc. jupyter nb helper functions + settings.
 
 import pandas as pd
+import numpy as np
 
 # output all lines, not just the last one, in each cell.
 from IPython.core.interactiveshell import InteractiveShell
@@ -9,11 +10,20 @@ InteractiveShell.ast_node_interactivity = 'all'
 # wider pd dataframe columns.
 pd.set_option('max_colwidth', 800)
 
-# pretty-print spark dataframe info.
+# pretty-print pd entiy or spark dataframe info.
 def pp(df, limit=10, raw=False):
-    print(df.schema)
-    print('count: ', df.count())
-    if raw:
-        return df.show(limit)
+    if type(df) == np.ndarray:
+        return pd.DataFrame(df).head(limit)
+    elif type(df) == pd.core.frame.DataFrame:
+        print(df.shape)
+        return df.head(limit)
+    elif type(df) == pd.core.series.Series:
+        print(df.shape)
+        return df.head(limit)
     else:
-        return df.limit(limit).toPandas().head(limit)
+        print(df.schema)
+        print('count: ', df.count())
+        if raw:
+            return df.show(limit)
+        else:
+            return df.limit(limit).toPandas().head(limit)
